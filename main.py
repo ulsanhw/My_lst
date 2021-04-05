18a5fa4b8a3d3fe1fdc4948ee26147388568386f
mmecoco <2000jooyoung@gmail.com>
2021-04-05 21:46:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
-pass
+L = myList()
+while True:
+    cmd = input().strip().split()
+    if cmd[0] == 'append':
+        L.append(int(cmd[1]))
+        print(f"  + {cmd[1]} is appended.")
+    elif cmd[0] == 'pop':
+        if len(cmd) == 1:
+            idx = -1
+        else:
+            idx = int(cmd[1])
+        try:
+            x = L.pop(idx)
+            print(f"  - {x} at {idx} is popped.")
+        except IndexError:
+            if len(L) == 0:
+                print("  ! list is empty.")
+            else:
+                print(f"  ! {idx} is an invalid index.")
+    elif cmd[0] == 'insert':
+        try:
+            L.insert(int(cmd[1]), int(cmd[2]))
+            print(f"  + {cmd[2]} is inserted at index {cmd[1]}.")
+        except IndexError:
+            print(f"  ! {cmd[1]} is an invalid index.")
+    elif cmd[0] == 'get': # getitem A[k]
+        try:
+            L[int(cmd[1])]
+            print(f"  @ L[{cmd[1]}] --> {L[int(cmd[1])]}.")
+        except IndexError:
+            print(f"  ! {cmd[1]} is an invalid index.")
+    elif cmd[0] == 'set': # setitem A[k] = x
+        try:
+            L[(int(cmd[1]))] = int(cmd[2])
+            print(f"  ^ L[{cmd[1]}] <-- {cmd[2]}.")
+        except IndexError:
+            print(f"  ! {cmd[1]} is an invalid index.")
+    elif cmd[0] == 'size':
+        print("  ? capacity =", L.size())
+    elif cmd[0] == 'print':
+        print(L)
+    elif cmd[0] == 'exit':
+        print('bye~')
+        break
+    else:
+        print(" ? invalid command! Try again.")
